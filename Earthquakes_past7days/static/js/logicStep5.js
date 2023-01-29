63c96ba1ec5f9a86834928e65196aadb3b99f87c
rrantxa <arantxa.meza@gmail.com>
2023-01-28 23:13:42 -0600

    Adding map legend.

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -97,3 +97,34 @@ d3.json(weekData).then(function(data) {
     // Then we add the earthquake layer to our map.
     earthquakes.addto(map);
 });
+
+// Create the layer control object.
+var legend = L.control({
+    position: 'bottomright'
+});
+
+legend.onAdd = function () {
+
+    let div = L.DomUtil.create('div', 'info legend');
+        const magnitudes = [0, 1, 2, 3, 4, 5];
+        const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+        ];
+
+    // loop through our intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
